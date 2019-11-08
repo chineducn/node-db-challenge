5ca029e336b0035c63efa16239f7256a5b793815
unknown <neduvinci@gmail.com>
2019-11-08 10:26:40 +0100

    Created project-resource table. executed migration and tested rollback

--- data/migrations/20191108095010_create-tables.js
+++ data/migrations/20191108095010_create-tables.js
@@ -44,7 +44,21 @@ exports.up = function(knex) {
                 .notNullable()
                 .defaultTo(0);
         })
-        .createTable('project_resource', )
+        .createTable('project_resource', tbl => {
+            tbl
+                .integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('project');
+            tbl
+                .integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('resource');
+            tbl.primary(['project_id', 'resource_id']);
+        })
 };
 
 exports.down = function(knex) {
