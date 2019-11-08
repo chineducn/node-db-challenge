b11f04f9415b5cafbc162e6c7c44c8cc0a0c0c95
unknown <neduvinci@gmail.com>
2019-11-08 09:54:42 +0100

    set up down function in migration file

--- data/migrations/20191108095010_create-tables.js
+++ data/migrations/20191108095010_create-tables.js
@@ -4,5 +4,10 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  
+    return knex
+        .schema
+        .dropTableIfExists('project_resource')
+        .dropTableIfExists('task')
+        .dropTableIfExists('resource')
+        .dropTableIfExists('project')
 };
