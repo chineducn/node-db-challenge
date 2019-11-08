93864cb332899158460fd8ee8e682fb93beb28e7
unknown <neduvinci@gmail.com>
2019-11-08 12:50:48 +0100

    Clean up in taskModel

--- models/taskModel.js
+++ models/taskModel.js
@@ -6,8 +6,17 @@ module.exports = {
 
 function getProjectTasks(project_id) {
     return db('task as t')
-        .join('project as p', 'p.id', 't.project_id')
-        .select('p.name as Project', 'p.description as Description', 't.description as Task', 't.completed as Task Completion Status')
+        .join(
+            'project as p',
+            'p.id',
+            't.project_id'
+        )
+        .select(
+            'p.name as Project',
+            'p.description as Project Description',
+            't.description as Task',
+            't.completed as Task Completion Status'
+        )
         .where({ project_id })
         .then(tasks => {
             return tasks.map(task => {
