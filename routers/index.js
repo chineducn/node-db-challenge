cff295531d9bba38090b5139ef6480f68acc24ea
unknown <neduvinci@gmail.com>
2019-11-08 11:56:46 +0100

    set up basic resource router

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const projectRouter = require('./projectRouter')
+const resourceRouter = require('./resourceRouter')
 
 const router = express.Router()
 
 router.use('/api/projects', projectRouter)
+router.use('/api/resources', resourceRouter)
 
 module.exports = router
