31362bfc7927c8643b4a8ed12e9eb636d196771b
unknown <neduvinci@gmail.com>
2019-11-08 10:04:01 +0100

    code for project table in migration file

--- data/migrations/20191108095010_create-tables.js
+++ data/migrations/20191108095010_create-tables.js
@@ -1,6 +1,23 @@
 
 exports.up = function(knex) {
-  
+    return knex
+        .schema
+        .createTable('project', tbl => {
+            tbl
+                .increments();
+            tbl
+                .string('name')
+                .notNullable();
+            tbl
+                .string('description')
+            tbl
+                .boolean('completed')
+                .notNullable()
+                .defaultTo(0)
+        })
+        .createTable()
+        .createTable()
+        .createTable()
 };
 
 exports.down = function(knex) {
