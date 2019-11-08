c787ede7975f1876d9baa560d8c8f7b8873d431e
unknown <neduvinci@gmail.com>
2019-11-08 12:15:13 +0100

    Added enpoint and function to add resource and get resource by id

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 const project = require('./projectModel')
+const resource = require('./resourceModel')
 
 module.exports = {
     getProjects: project.getProjects,
     getProjectById: project.getProjectById,
     addProject: project.addProject,
+    getResources: resource.getResources,
+    getResourceById: resource.getResourceById,
+    addResource: resource.addResource,
+
 }
